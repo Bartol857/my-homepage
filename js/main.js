cdb7a3ae59be1fd07ddffa349bc67dbe183803ee
Bartek Kokoszko <45509480+Bartol857@users.noreply.github.com>
2025-02-17 13:00:46 +0100

    Update main.js
    Modify openweathermap API

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ let popupCloseBtn;
 
 //API
 const API_LINK = 'https://api.openweathermap.org/data/3.0/weather?q=';
-const API_KEY = '&appid=42e6ffb4deb2d0bfe5ec46ce0ea312e2';
+const API_KEY = '&appid=25db98b4baeaae8f3186cd3bb3a1503b';
 const API_UNITS = '&units=metric';
 const URL =	'https://api.sunrisesunset.io/json?lat=53.1824306&lng=-22.0521838&date=today';
 
